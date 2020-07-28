ff004ca1f27c35ffd790c68f3101e9bb0ddf5f8e
flakster <carlos.santamaria@gmail.com>
2020-07-28 15:49:45 -0500

    Add the style-loader and css-loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("all right")
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const content = document.getElementById("content");
+const header = document.createElement("h1");
+header.innerHTML = "Vegan delights restaurant";
+content.appendChild(header);
