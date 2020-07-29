eac0f1a1c0f4dfa7df050b7cf0e2a90802bc4839
flakster <carlos.santamaria@gmail.com>
2020-07-29 12:14:46 -0500

    Add background, fonts and menu bar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import background from './background.jpg';
+import './style.css';
+import './GreatVibes-Regular.ttf'
 
 const content = document.getElementById("content");
+
+const container = document.createElement('div');
+container.classList.add('fixed-background');
+window.addEventListener('scroll',function(){
+  showMenu();
+});
+
 const header = document.createElement("h1");
-header.innerHTML = "Vegan delights restaurant";
-content.appendChild(header);
+header.classList.add('text-center','position-relative', 'text-white', 'header');
+header.innerHTML = "Vegan delights";
+container.appendChild(header);
+
+const bgimage = new Image();
+bgimage.src = background;
+
+const navbar = document.createElement('div');
+navbar.classList.add('bg-success','fixed-top', 'd-none','menu');
+navbar.innerHTML=" ***** HERE *****";
+container.appendChild(navbar);
+
+content.appendChild(container);
+
+function showMenu(){
+  if (window.scrollY>0){
+
+    navbar.classList.remove('d-none');
+  }
+  else
+  {
+    navbar.classList.add('d-none');
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,19 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ]
   }
 };
